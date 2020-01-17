bc4551153ffb96b4f0d9f0e144a2618bd8eb8b9e
0zz1cl13 <seniorcl13@gmail.com>
2020-01-17 13:52:58 -0400

    projeto hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+
+export default function App() {
+  const [location, setLocation] = useState({});
+
+  useEffect(() => {
+    navigator.geolocation.watchPosition(handlePositionReceived)
+  }, []);
+
+  function handlePositionReceived({ coords }) {
+    const { latitude, longitude } = coords;
+    
+    setLocation({ latitude, longitude});
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      Latitude: {location.latitude} <br />
+      Longitude: {location.longitude} 
+    </>
+  )
 
-export default App;
+  // const [repositories, setRepositories] = useState([]);
+
+  // useEffect( () => {
+  //   async function getRepositories() {
+  //     const response = await fetch('https://api.github.com/users/diego3g/repos');
+  //     const data = await response.json();
+  //     console.log(data);
+  //     setRepositories(data);
+  //   }
+  //   getRepositories();
+  // }, []);
+
+  // useEffect(() => {
+  //   const filtered = repositories.filter(repo => repo.favorite);
+
+  //   document.title = `${filtered.length} favoritos`;
+  // }, [repositories])
+
+  // function handleFavorite(id) {
+  //   const newRepositories = repositories.map(repo => {
+  //     return id === repo.id ? { ...repo, favorite: !repo.favorite } : repo;
+  //   });
+
+  //   setRepositories(newRepositories);
+  // }
+
+  // const [repositories, setRepositories] = useState([
+  //   {id: 1, name: 'repo-1'},
+  //   {id: 2, name: 'repo-2'},
+  //   {id: 3, name: 'repo-3'},
+  // ]);
+  
+  // function handleAddRepository() {
+  //   setRepositories([...repositories, { id: Math.random(), name: 'Novo repo'}]);   
+  // } 
+
+  // return (
+  //   <>
+  //     <ul>
+  //       {repositories.map(repo => (
+  //         <li key={repo.id}>
+  //           {repo.name}
+  //           {repo.favorite && <span>(Favorito)</span>}
+  //           <button onClick={() => handleFavorite(repo.id)}>Favoritar</button>
+  //         </li> 
+  //       ))}
+  //     </ul>
+  //     {/* <button onClick={handleAddRepository}>
+  //       Adicionar repositório
+  //     </button> */}
+  //   </>
+  // );
+
+}
